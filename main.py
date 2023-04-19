8049d9448f3a42731b72f8f1de4d37d5e47695b5
BenniBriggs <130768079+BenniBriggs@users.noreply.github.com>
2023-04-18 20:24:39 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+/
+
+class BenniBriggs:
+    pass
+
+
+Users/BenniBriggs/PycharmProjects/"The Rubik's Goldberg Machine"/venv/bin/python /Users/BenniBriggs/PycharmProjects/"The Rubik's Goldberg Machine"/main.py
+/Users/BenniBriggs/PycharmProjects/"The Rubik's Goldberg Machine"/venv/bin/python /Users/BenniBriggs/PycharmProjects/"The Rubik's Goldberg Machine"/main.py
